e28dab36eb2e25ab8924515c99b2430733b90b65
Diego1223 <diegonn2555@gmail.com>
2021-11-05 19:20:31 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿//Password generator
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //It has 76 characters
+        static char[] characters = new char[76] {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+
+            '!', '@', '#', '$', '%', '^', '&', '*', '(',
+            ')', '-', '_', '=', '+',
+
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+        };
+
+        static void Main(String[] args)
+        {
+            byte length;
+
+            CentrarTexto("Password Generator", 2);
+
+            System.Console.SetCursorPosition(0, 5);
+            Console.Write("How many characters will have your new password => ");
+            length = Convert.ToByte(Console.ReadLine());
+
+            GeneratePassword(length);
+
+            Console.ReadKey();
+        }
+
+        static void GeneratePassword(byte length)
+        {
+            char[] password = new char[length];
+
+            int random;
+            Random num = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                random = num.Next(0, 76);
+                password[i] = characters[random];
+            }
+
+            LoadScreen();
+
+            Console.WriteLine("We already have your new password! \n");
+
+            for(int i = 0; i < password.Length; i++)
+            {
+                Console.Write(password[i]);
+            }
+        }
+        static void CentrarTexto(string texto, int y)
+        {
+            float size;
+            size = texto.Length;
+
+            System.Console.SetCursorPosition((int)(59.5 - (size / 2)), y);
+            Console.Write(texto);
+        }
+
+        static void LoadScreen()
+        {
+            byte counter = 0;
+
+            while (counter < 2)
+            {
+                Console.WriteLine("Please wait .  /");
+                Thread.Sleep(500);
+                System.Console.Clear();
+
+                Console.WriteLine("Please wait .. |");
+                Thread.Sleep(500);
+                System.Console.Clear();
+
+                Console.WriteLine("Please wait ...\\");
+                Thread.Sleep(500);
+                System.Console.Clear();
+
+                counter++;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Test.cs
@@ -0,0 +1,72 @@
+﻿
+
+
+///*
+// * Programa que calcule el minimo comun multiple
+// */
+
+///*
+// * Nociones basicas
+// * Dividir
+// * Multiplicar
+// * No se aceptan valores con punto
+// * Multiplicar todos los valores encontrados al final
+// * 
+// * mcm(25, 75) = 75
+// * 
+// *  25 75 | 3
+// *  25 75 | 5
+// *  25 25 | 5
+// *  5   5 |
+// *  1   1 |
+// */
+//using System;
+
+//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+
+//        static void Main(String[] args)
+//        {
+//            //Numeros primos = 2, 3, 5, 7
+
+//            Dividir("25", 2);
+
+//            Console.ReadKey();
+//        }
+
+//        static void Dividir(string valor, int dividirPor)
+//        {
+//            char[] recorrer;
+//            bool entero = true;
+//            double aux = 0;
+//            string n1;
+
+//            n1 = valor;
+//            aux = Convert.ToDouble(n1);
+//            aux = aux / dividirPor;
+
+//            n1 = Convert.ToString(aux);
+//            recorrer = n1.ToCharArray();
+
+//            for (int i = 0; i < 4; i++)
+//            {
+//                if (recorrer[i] == '.')
+//                {
+//                    entero = false;
+//                }
+//            }
+
+//            if (entero)
+//            {
+//                Convert.ToInt32(n1);
+//                Console.WriteLine("Es un numero entero.");
+//            }
+//            else
+//            {
+//                Convert.ToDouble(n1);
+//            }
+//        }
+//    }
+//}
